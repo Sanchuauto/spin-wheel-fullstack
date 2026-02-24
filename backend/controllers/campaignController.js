bdfed59340196c41c9005244a5c258708ce849ef
Sanchuauto <automail@sanchu.org.in>
2026-02-24 22:46:49 +0530

    fix uuid esm crash for vercel

--- backend/controllers/campaignController.js
+++ backend/controllers/campaignController.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 // @desc    Create a new campaign
 // @route   POST /api/admin/campaigns
